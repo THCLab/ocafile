7a6024e53d5ed4994f26e7360779b4f4041c0407
Robert Mitwicki <robert@thclab.online>
2023-04-17 12:51:11 +0200

    Use indexmap to assure about ordering

--- ocaast/src/lib.rs
+++ ocaast/src/lib.rs
@@ -1,7 +1,7 @@
+use indexmap::IndexMap;
 use serde::{Serialize, Serializer};
 use strum_macros::Display;
 
-use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Serialize)]
 pub struct OCAAst {
@@ -84,22 +84,21 @@ pub enum OverlayType {
 
 #[derive(Debug, PartialEq, Serialize)]
 pub struct CaptureBaseContent {
-    pub attributes: Option<HashMap<String, NestedValue>>,
-    pub properties: Option<HashMap<String, NestedValue>>,
+    pub attributes: Option<IndexMap<String, NestedValue>>,
+    pub properties: Option<IndexMap<String, NestedValue>>,
 }
 
 #[derive(Debug, PartialEq, Serialize)]
-pub struct OverlayContent {
-    pub capture_base_id: Option<String>, // TODO do we need it in AST?
-    pub properties: Option<HashMap<String, NestedValue>>,
-    pub body: Option<HashMap<String, NestedValue>>, // maybe we should have body and attributes
+pub struct OverlayContent { // maybe we could unify it with CaptureBaseContent?
+    pub properties: Option<IndexMap<String, NestedValue>>,
+    pub body: Option<IndexMap<String, NestedValue>>, // maybe we should have body and attributes
 }
 
 #[derive(Debug, PartialEq, Serialize)]
 #[serde(untagged)]
 pub enum NestedValue {
     Value(String),
-    Object(HashMap<String, NestedValue>),
+    Object(IndexMap<String, NestedValue>),
     Reference(String),
     Array(Vec<NestedValue>),
 }
@@ -138,13 +137,13 @@ mod tests {
 
     #[test]
     fn test_ocaast_serialize() {
-        let mut attributes = HashMap::new();
-        let mut properties = HashMap::new();
-        let mut person = HashMap::new();
+        let mut attributes = IndexMap::new();
+        let mut properties = IndexMap::new();
+        let mut person = IndexMap::new();
         person.insert("name".to_string(), NestedValue::Value("Text".to_string()));
 
-        attributes.insert("person".to_string(), NestedValue::Object(person));
         attributes.insert("test".to_string(), NestedValue::Value("test".to_string()));
+        attributes.insert("person".to_string(), NestedValue::Object(person));
         properties.insert("test".to_string(), NestedValue::Value("test".to_string()));
         let command = Command {
             kind: CommandType::Add,
